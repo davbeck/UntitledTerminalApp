096e8fa1e558bf74c7ba5addbe2498a306d8b040
David Beck <code@davidbeck.co>
2024-04-07 20:36:02 -0700

    Add insets to prompt text view

--- Terminal/Views/Prompt/STTextViewRepresentable.swift
+++ Terminal/Views/Prompt/STTextViewRepresentable.swift
@@ -1,51 +1,46 @@
-//
-//  STTextViewRepresentable.swift
-//  Terminal
-//
-//  Created by David Beck on 4/2/24.
-//
-
-import SwiftUI
-import STTextView
 import AppKit
 import Combine
+import STTextView
+import SwiftUI
 
 struct STTextViewRepresentable: NSViewRepresentable {
 	typealias NSViewType = NSScrollView
-	
+
 	@Binding var text: String
-	
+
 	func makeCoordinator() -> Coordinator {
 		Coordinator(parent: self)
 	}
-	
+
 	func makeNSView(context: Context) -> NSViewType {
 		let scrollView = TextView.scrollableTextView()
+
+		scrollView.automaticallyAdjustsContentInsets = false
+		scrollView.contentInsets = .init(top: 5, left: 8, bottom: 5, right: 8)
+
 		scrollView.hasHorizontalScroller = false
 		if let textView = scrollView.documentView as? STTextView {
 			textView.delegate = context.coordinator
 			textView.widthTracksTextView = false
-//			textView.isHorizontallyResizable = false
 			textView.heightTracksTextView = true
-//			textView.isVerticallyResizable = false
 		}
-		
+
 		return scrollView
 	}
-	
+
 	func updateNSView(_ scrollView: NSViewType, context: Context) {
 		context.coordinator.parent = self
-		
+
 		guard let textView = scrollView.documentView as? STTextView else { return }
-		
+
 		if textView.string != text {
 			textView.string = text
 		}
 	}
-	
+
 	func sizeThatFits(_ proposal: ProposedViewSize, nsView scrollView: NSViewType, context: Context) -> CGSize? {
 		guard let textView = scrollView.documentView as? STTextView else { return nil }
-		
+
 		var maxFragmentY: CGFloat = 0
 		textView.textLayoutManager.enumerateTextLayoutFragments(
 			from: nil,
@@ -54,14 +49,17 @@ struct STTextViewRepresentable: NSViewRepresentable {
 			maxFragmentY = max(maxFragmentY, fragment.layoutFragmentFrame.maxY)
 			return true
 		}
-		
-		return CGSize(width: proposal.width ?? textView.intrinsicContentSize.width, height: maxFragmentY)
+
+		return CGSize(
+			width: proposal.width ?? textView.intrinsicContentSize.width,
+			height: scrollView.contentInsets.top + maxFragmentY + scrollView.contentInsets.bottom
+		)
 	}
-	
+
 	@MainActor
 	class Coordinator {
 		var parent: STTextViewRepresentable
-		
+
 		init(parent: STTextViewRepresentable) {
 			self.parent = parent
 		}
@@ -74,7 +72,7 @@ extension STTextViewRepresentable.Coordinator: STTextViewDelegate {
 			guard let textView = notification.object as? STTextView else {
 				return
 			}
-			
+
 			if self.parent.text != textView.string {
 				self.parent.text = textView.string
 			}
